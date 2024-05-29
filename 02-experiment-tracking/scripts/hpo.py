db2f1811845cd067afbfbb177741c4a787aa1da7
Agnes Olorundare <ojuhagnes@gmail.com>
2024-05-29 09:46:20 +0000

    A commit.

--- 02-experiment-tracking/scripts/hpo.py
+++ 02-experiment-tracking/scripts/hpo.py
@@ -1,4 +1,4 @@
-mport os
+import os
 import pickle
 import click
 import mlflow
@@ -35,12 +35,14 @@ def run_optimization(data_path: str, num_trials: int):
 
     def objective(params):
 
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        with mlflow.start_run():
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            mlflow.log_metric("rmse", rmse)
 
-        return {'loss': rmse, 'status': STATUS_OK}
+            return {'loss': rmse, 'status': STATUS_OK}
 
     search_space = {
         'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
--- 02-experiment-tracking/scripts/train.py
+++ 02-experiment-tracking/scripts/train.py
@@ -5,6 +5,14 @@ import click
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+import mlflow
+
+mlflow.autolog(disable=True)
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+mlflow.set_experiment("02-nyc-taxi-experiment")
+
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -19,15 +27,16 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.start_run():
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 if __name__ == '__main__':
     run_train()
